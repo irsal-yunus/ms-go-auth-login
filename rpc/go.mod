155fefb5226db4df4f128c03789e6a1a821c8248
irsal.yunus04 <irsal.yunus04@gmail.com>
2021-10-27 14:34:50 +0700

    delete dir

--- rpc/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/irsal-yunus/ms-go-auth-login/rpc
-
-go 1.16
--- task/data/task.model.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type Task struct {
-	ID       int    `json:"id" uri:"id", form:"id"`
-	Name     string `json:"name" form:"name"`
-	Employee int    `json:"employee" form:"employee"`
-}
