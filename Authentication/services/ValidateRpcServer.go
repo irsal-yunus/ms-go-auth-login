87cdd77cdf3578b1ba15faf64b662a3f65ae88d4
irsal.yunus04 <irsal.yunus04@gmail.com>
2021-10-27 14:35:15 +0700

    Revert "Delete Dir Authentications"
    This reverts commit bc8dc0f467a0c0617554e361b4805e7f6f59eada.

---
+++ Authentication/services/ValidateRpcServer.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/irsal-yunus/ms-go-auth-login/Authentication/models"
+	rpc_auth "github.com/irsal-yunus/ms-go-auth-login/rpc/rpc_auth"
+)
+
+type ValidateRpcServer struct {
+	rpc_auth.UnimplementedValidateTokenServiceServer
+}
+
+func (ValidateRpcServer) Validate(stream rpc_auth.ValidateTokenService_ValidateServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		logger := log.New(os.Stdout, "validateRpc", 1)
+		flags, errF := models.GetFlags()
+		if errF != nil {
+			return errF
+		}
+		service := NewLogin(logger, flags)
+
+		valid, claims := service.VerifyToken(in.Token, "")
+
+		if !valid {
+			if err := stream.Send(&rpc_auth.ValidateTokenResponse{
+				IsValid:   valid,
+				ComapnyId: "",
+				Username:  "",
+				Roles:     nil,
+			}); err != nil {
+				return err
+			}
+		} else {
+			if err := stream.Send(&rpc_auth.ValidateTokenResponse{
+				IsValid:   valid,
+				ComapnyId: claims.ComapnyId,
+				Username:  claims.Username,
+				Roles:     nil,
+			}); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
